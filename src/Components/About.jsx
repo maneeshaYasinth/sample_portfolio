46bea8547a9da954a57de54b6cc9b783986470c4
Maneesha Yasinth Gunarathna <130350320+maneeshaYasinth@users.noreply.github.com>
2024-02-03 18:42:17 +0000

    done some changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that;
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a novice developer at the University of Kelaniya in Sri Lanka, reveling in the creation of programs.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am enthusiastic about tackling programming challenges with innovative solutions to foster creativity. Drawing on my expertise in coding and software development, I consistently explore novel approaches to enhance accessibility and usability, aiming to make technology more inclusive for everyone.";
 
 const About = () => {
   return (
